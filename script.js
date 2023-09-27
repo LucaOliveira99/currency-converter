0056356f2aff4c98da39612ff0ce21f28ab7f7c8
LucaOliveira <lucasotooliveira@gmail.com>
2023-09-27 20:43:52 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,52 @@
+let apiKey = "2825ea619c606e6b2ecf1564";
+let api = `https://v6.exchangerate-api.com/v6/${apiKey}/latest/USD`;
+const fromDropDown = document.getElementById("from-currency-select");
+const toDropDown = document.getElementById("to-currency-select");
+
+//Criar dropdown através do currencies array
+currencies.forEach((currency) => {
+  const option = document.createElement("option");
+  option.value = currency;
+  option.text = currency;
+  fromDropDown.add(option);
+});
+
+//Repeat same thing for the other dropdown
+currencies.forEach((currency) => {
+  const option = document.createElement("option");
+  option.value = currency;
+  option.text = currency;
+  toDropDown.add(option);
+});
+
+//Setting default values
+fromDropDown.value = "EUR";
+toDropDown.value = "BRL";
+
+let convertCurrency = () => {
+  //Create References
+  const amount = document.querySelector("#amount").value;
+  const fromCurrency = fromDropDown.value;
+  const toCurrency = toDropDown.value;
+
+  //If amount input field is not empty
+  if (amount.length != 0) {
+    fetch(api)
+      .then((resp) => resp.json())
+      .then((data) => {
+        let fromExchangeRate = data.conversion_rates[fromCurrency];
+        let toExchangeRate = data.conversion_rates[toCurrency];
+        const convertedAmount = (amount / fromExchangeRate) * toExchangeRate;
+        result.innerHTML = `${amount} ${fromCurrency} = ${convertedAmount.toFixed(
+          2
+        )} ${toCurrency}`;
+      });
+  } else {
+    alert("Por favor preencha o Valor");
+  }
+};
+
+document
+  .querySelector("#convert-button")
+  .addEventListener("click", convertCurrency);
+window.addEventListener("load", convertCurrency);
